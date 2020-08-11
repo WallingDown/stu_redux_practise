0bece5c3a20b8b00a9ff902d3c70bdcb298b99ac
WallingDown <1532147890@qq.com>
2020-08-11 21:36:58 +0800

    递加算法之纯react版

---
+++ src_递加算法之纯react版/components/Count.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+    state = {
+        count:0,
+        name:'丹丹'
+    }
+
+    // 加法
+    increment = () =>{
+        // 获取用户输入
+        const {value} = this.refs.selectedNumber
+        // 获取原状态
+        const {count} = this.state
+        this.setState({count:count + value*1})
+        
+    }
+    // 减法
+    decrement = () =>{
+         // 获取用户输入
+         const {value} = this.refs.selectedNumber
+         // 获取原状态
+         const {count} = this.state
+        //  更新状态
+         this.setState({count:count - value*1})
+    }
+    // 奇数加
+    incrementIfOdd = () =>{
+        if (this.state.count % 2 !== 0) {
+            const {value} = this.refs.selectedNumber
+            const {count} = this.state
+            this.setState({count:count + value*1})
+        }
+    }
+    // 等一等加
+    incrementWait = () =>{
+        setTimeout(()=>{
+            const {value} = this.refs.selectedNumber
+            const {count} = this.state
+            this.setState({count:count + value*1})
+        },500)
+    }
+
+    render() {
+        return (
+            <div style={{marginLeft:'500px'}}>
+                <h1>{this.state.name}计算得结果为{this.state.count}</h1>
+                <select ref="selectedNumber">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+                &nbsp;&nbsp;&nbsp;
+                <button onClick={this.increment}>+</button>
+                &nbsp;&nbsp;&nbsp;
+                <button onClick={this.decrement}>-</button>
+                &nbsp;&nbsp;&nbsp;
+                <button onClick={this.incrementIfOdd}>奇数 +</button>
+                &nbsp;&nbsp;&nbsp;
+                <button onClick={this.incrementWait}>等一等再 +</button>
+                &nbsp;&nbsp;&nbsp;
+            </div>
+        )
+    }
+}
